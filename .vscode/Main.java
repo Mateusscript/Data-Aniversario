e3e34127f2720c23f3774f71c6668174c6dee42c
Mateusscript <mateusgamesgff@gmail.com>
2024-11-28 11:25:03 -0300

    Contador de idade

---
+++ .vscode/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int idade = 18;
+        System.out.println("Minha idade: " + idade);
+
+        int idadeProxAno;
+        idadeProxAno = idade + 1;
+
+        System.out.println("Minha idade no próximo ano: " + idadeProxAno);
+    }
+}
